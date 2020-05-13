826e4d3981cd7050941a9f85bc5c6d56f758923b
latifahd <64581083+latifahd@users.noreply.github.com>
2020-05-13 15:44:22 +0700

    Create latihan3.php

---
+++ get-and-post/latihan3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>POST</title>
+</head>
+<body>
+	<form action="latihan4.php" method="post">
+		Masukkan nama :
+		<input type="text" name="nama">
+		<br>
+		<button type="submit" name="submit">Kirim!</button>
+	</form>
+</body>
+</html>
