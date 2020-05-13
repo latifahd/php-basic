459518d5d8ba11030e277889dbf1a56ac85da55f
latifahd <64581083+latifahd@users.noreply.github.com>
2020-05-14 06:01:01 +0700

    Update script.js

--- live-search-jquery/js/script.js
+++ live-search-jquery/js/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
+	// hilangkan tombol cari
+	$('#tombol-cari').hide();
+
 	// event ketika keyword ditulis
-	$('keyword').on('keyup', function() {
-		$('container').load('ajax/buku.php?keyword=' + $('#keyword').val())
-	})
+	$('#keyword').on('keyup', function() {
+		// munculkan icon loading
+		$('.loader').show();
+
+		// ajax menggunakan load
+		// $('container').load('ajax/buku.php?keyword=' + $('#keyword').val())});
+
+		// $.get
+		$.get('ajax/buku.php?keyword=' + $('keyword').val(), function(data) {
+			$('#container').html(data);
+			$('.loader').hide();
+		}); 
 });
