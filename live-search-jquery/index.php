b97bfc7f69cfe02b8120aedfce0e39e4522e5b5e
latifahd <64581083+latifahd@users.noreply.github.com>
2020-05-14 05:22:11 +0700

    Add files via upload

---
+++ live-search-jquery/index.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+	header("Location: login.php");
+	exit;
+}
+
+require 'functions.php';
+$buku = query("SELECT * FROM buku");
+
+// tombol cari ditekan
+if (isset($_POST["cari"])) {
+	$buku = cari($_POST["keyword"]);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Admin</title>
+</head>
+<body>
+	<a href="logout.php">Logout</a>
+	<h1>Daftar Buku</h1>
+
+	<a href="tambah.php">Tambah data buku</a>
+	<br><br>
+
+	<form action="" method="post">
+		<input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian.." autocomplete="off" id="keyword">
+		<button type="submit" name="cari" id="tombol-cari">Cari!</button>
+	</form>	
+	<br>
+	<div id="container">
+	<table border="1" cellpadding="10" cellspacing="0">
+		<tr>
+			<th>No.</th>
+			<th>Aksi</th>
+			<th>Gambar</th>
+			<th>Judul</th>
+			<th>Penulis</th>
+			<th>Tebal</th>
+			<th>ISBN</th>
+		</tr>
+		
+		<?php $i=1; ?>
+		<?php foreach( $buku as $row) : ?>
+		<tr>
+			<td><?= $i; ?></td>
+			<td>
+				<a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
+				<a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
+			</td>
+			<td><img src="img/<?= $row["gambar"] ?>" width="50"></td>
+			<td><?= $row["judul"]; ?></td>
+			<td><?= $row["penulis"]; ?></td>
+			<td><?= $row["tebal"]; ?></td>
+			<td><?= $row["isbn"]; ?></td>	
+		</tr>
+		<?php $i++; ?>
+		<?php endforeach; ?>
+
+	</table>
+	</div>
+	<script src="js/jquery-3.5.1.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
---
+++ live-search-jquery/tambah.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+	header("Location: login.php");
+	exit;
+}
+
+require 'functions.php';
+
+// cek apakah tombol submit sudah ditekan
+if(isset ($_POST["submit"]) ) {
+	
+
+	
+
+	// cek apakah data berhasil ditambahkan
+	if( tambah($_POST) > 0) {
+		echo "
+			<script>
+				alert('data berhasil ditambahkan');
+				document.location.href = 'index.php';
+			</script>
+		";
+	} else {
+		echo "
+			<script>
+				alert('data gagal ditambahkan');
+				document.location.href = 'index.php';
+			</script>
+		";
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tambah data mahasiswa</title>
+</head>
+<body>
+	<h1>Tambah data mahasiswa</h1>
+
+	<form action="" method="post" enctype="multipart/form-data">
+		<ul>
+			<li>
+				<label for="judul">Judul : </label>
+				<input type="text" name="judul" id="judul" required>
+			</li>
+			<li>
+				<label for="penulis">Penulis : </label>
+				<input type="text" name="penulis" id="penulis" required>
+			</li>
+			<li>
+				<label for="tebal">Tebal : </label>
+				<input type="text" name="tebal" id="tebal"required>
+			</li>
+			<li>
+				<label for="isbn">ISBN : </label>
+				<input type="text" name="isbn" id="isbn" required>
+			</li>
+			<li>
+				<label for="gambar">Gambar : </label>
+				<input type="file" name="gambar" id="gambar"required>
+			</li>
+			<li>
+				<button type="submit" name="submit">Tambah data</button>
+			</li>
+
+		</ul>
+
+	</form>
+</body>
+</html>
